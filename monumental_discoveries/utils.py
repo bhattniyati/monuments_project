67de257289140ae95f557cc8bee78cc2cfadb971
razmeenm <mirzarazmeen@gmail.com>
2024-01-22 12:13:25 +0530

    [UPDATED] - registration views

--- monumental_discoveries/utils.py
+++ monumental_discoveries/utils.py
@@ -1,6 +1,9 @@
 """
 Keep utils functions
 """
+from .models import User
+
+import re
 
 def validate_data(request_data: dict):
     """
@@ -18,13 +21,36 @@ def validate_data(request_data: dict):
         "cpassword": "Confirm password",
     }
 
+    # Email validation regex pattern
+    email_pattern = re.compile(r'^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$')
+
     for field_name, display_name in required_fields.items():
-        if not request_data.get(field_name):
+        value = request_data.get(field_name, "").strip()
+
+        if not value:
             error_list.append(f"{display_name} is required")
+        elif field_name == "email" and not email_pattern.match(value):
+            error_list.append("Invalid email format")
+        elif field_name == "password" and len(value) < 8:
+            error_list.append("Password must be at least 8 characters long")
 
     if str(request_data["password"]).lower() != str(request_data["cpassword"]).lower():
         error_list.append("Password and confirm password are not the same")
 
+    if len(request_data["phoneno"]) != 10:
+        error_list.append("Invalid phone number. Phone number must be 10 digits long")
+
+    if request_data.get("email"):
+        email_data = User.objects.filter(email=request_data["email"]).first()
+        if email_data:
+            error_list.append("This email is already taken, please try with a different email id")
+
+    if request_data.get("uname"):
+        username_data = User.objects.filter(username=request_data["uname"]).first()
+        if username_data:
+            error_list.append("This username is already taken, please try with a different username")
+
     if error_list:
         return False, error_list
+
     return True, error_list
--- monumental_discoveries/views.py
+++ monumental_discoveries/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,HttpResponseRedirect, redirect
 from django.contrib import messages
 from .models import User
-from django.urls import reverse
 from .utils import validate_data
+from django.contrib.auth.hashers import make_password
+
 
 
 # Create your views here.
@@ -12,24 +13,25 @@ def home(request):
 def signup(request):
     if request.POST:
         status, error_list = validate_data(request.POST)
+
         if status:
+            hashed_password = make_password(request.POST["password"])
             User.objects.create(first_name=request.POST["fname"],last_name=request.POST["lname"],
                                 email=request.POST["email"], phone_no=request.POST["phoneno"],
-                                password=request.POST["password"], username=request.POST["uname"],
+                                password=hashed_password, username=request.POST["uname"],
                                 gender=request.POST["gender"]
                                 )
             return redirect("home")
 
-        # if len(error_list) == 1:
-        #     message = error_list[0]
-        # elif len(error_list) == 2:
-        #     message = str(error_list[0]) + " and " + str(error_list[1])
-        # else:
-        #     comma_separated = ", ".join(error_list[:-1])
+        if len(error_list) == 1:
+            message = error_list[0]
+        elif len(error_list) == 2:
+            message = str(error_list[0]) + " and " + str(error_list[1])
+        else:
+            comma_separated = ", ".join(error_list[:-1])
+            message = f"{comma_separated} and {error_list[-1]}"
 
-        # message = f"{comma_separated} and {error_list[-1]}"
-
-        messages.error(request, error_list)
+        messages.error(request, message)
         return redirect('signup')
 
     return render(request, "registration.html")
